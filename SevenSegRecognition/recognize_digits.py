be0069ebfacc6a448c05cebf8af3800084ff06ec
Jared Barfknecht <barfknechtj@outlook.com>
2017-07-05 21:16:15 -0700

    Initial commit and environment config for recognition of numbers in seven segment display

---
+++ SevenSegRecognition/recognize_digits.py
@@ -0,0 +1,8 @@
+# import packages
+# dependencies needed for packages: numpy, scipy
+from imutils.perspective import four_point_transform
+from imutils import contours
+import imutils
+import cv2
+
+print("packages imported correctly")
